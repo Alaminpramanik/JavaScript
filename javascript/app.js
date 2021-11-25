3de9835259ab1dc9e24717bc1a2998ff8235ac27
alaminpramanik <alamin493641@gmail.com>
2021-11-25 10:32:26 +0600

    javascript number, string, replace,split,concat, slice, lowerecase, uppercase,html display

---
+++ javascript/app.js
@@ -0,0 +1,19 @@
+// alert("Are you want generate your business name.")
+// var number= 20
+// var number2= 20
+// var total= number+number2
+// total++;
+// total++;
+// total++;
+// total--;
+// total--;
+// // alert(total);
+// alert(total);
+var alamin = "i'm a Junior developer"
+var towhid = "He is a Junior developer"
+var business= "<h1>" +alamin  + ' ' +towhid + "</h1>";
+// var business = document.write("If you're looking for an easy-to-use business name creator, Shopify's brand naming generator will help you name your business or ecommerce store in 10-seconds")
+document.write(business)
+// console.log('alert',business)
+// var title="jjkkjd"
+// document.getElementById('busines').innerHTML= title;
