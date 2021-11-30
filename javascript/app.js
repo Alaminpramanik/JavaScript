04166cf219c64eba212282be32f15bdc7bb154f9
alaminpramanik <alamin493641@gmail.com>
2021-11-30 10:19:10 +0600

    javascript array object and slice and tosorting and function

--- javascript/app.js
+++ javascript/app.js
@@ -11,9 +11,16 @@
 // alert(total);
 var alamin = "i'm a Junior developer"
 var towhid = "He is a Junior developer"
-var business= "<h1>" +alamin  + ' ' +towhid + "</h1>";
+// var business= "<h1>" +alamin  + ' ' +towhid + "</h1>";
 // var business = document.write("If you're looking for an easy-to-use business name creator, Shopify's brand naming generator will help you name your business or ecommerce store in 10-seconds")
-document.write(business)
+// document.write(business)
 // console.log('alert',business)
 // var title="jjkkjd"
 // document.getElementById('busines').innerHTML= title;
+
+function JsFunction(){
+    var business= "<h1>" +alamin  + ' ' +towhid + "</h1>";
+    document.getElementById('business').innerHTML= business;
+    console.log('business', business)
+}
+JsFunction();
